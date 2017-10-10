ed4895516d1433ec64feca91533baedd581858b5
Gerald McAuley <geraldmcauley@gmail.com>
2017-10-10 09:45:14 +0100

    Update ScreenShotFailureTest.py

--- ScreenShotFailureTest.py
+++ ScreenShotFailureTest.py
@@ -10,6 +10,7 @@ class ScreenSotOnFailure(unittest.TestCase):
     def test_example_1(self):
         self.driver = webdriver.Firefox()
         driver = self.driver
+        #error handling
         try:
             driver.get('https://www.google.ie/search?biw=1280&bih=632&tbm=nws&q=stocks&oq=stocks&gs_l=psy-ab.3..0l4.3113.8765.0.9340.8.8.0.0.0.0.123.409.3j1.6.0....0...1.1.64.psy-ab..2.4.408.0...97.fKpNb97lzXs')
             driver.save_screenshot('screenie.png')
