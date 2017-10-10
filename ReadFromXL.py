32cd18dcbf6ed7eb85c104b7ce326978c0ea84df
Gerald McAuley <geraldmcauley@gmail.com>
2017-10-10 16:25:40 +0100

    Update ReadFromXL.py

--- ReadFromXL.py
+++ ReadFromXL.py
@@ -1,11 +1,17 @@
+#Script allows you to read data from a spreadsheet
+#Gerald McAuley 2017
+#!!Important!!
+#Spreadsheet must be locatedin the same directory as the python file
 import xlrd
 
-xl_workbook = xlrd.open_workbook('sheet.xls')
+xl_workbook = xlrd.open_workbook('sheet.xls')#insert name of your spreadsheet here
 
+#Selects a specific page in spredsheet
 xl_sheet = xl_workbook.sheet_by_index(1)
 
 print ('Sheet name: %s' % xl_sheet.name)
 
+#Retrieve data from specific column/row
 col= xl_sheet.col(9)
 urlFile = open("urlFile.txt", "w")
 
