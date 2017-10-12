9dd3c2a9701b158bbe88bace7b9b61f664fa9e52
Gerald McAuley <geraldmcauley@gmail.com>
2017-10-12 15:57:09 +0100

    Update WalkingDirectory.py

--- WalkingDirectory.py
+++ WalkingDirectory.py
@@ -1,7 +1,8 @@
+#Automate the boring stuff with Python Chapter 9
 #Script that searches a given directory and lists all the subfolders and files within
 import os
 
-for folderName, subfolders, filenames in os.walk('/Users/gmcauley/Desktop/Code/'):#Add directory here
+for folderName, subfolders, filenames in os.walk('/Users/JoeBloggs/Desktop/FolderXYZ/'):#Add directory here
     print('The current folder is ' + folderName)
 
     for subfolder in subfolders:
