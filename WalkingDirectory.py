6d4ae035cd6a327b84852262dabbefde58572795
Gerald McAuley <geraldmcauley@gmail.com>
2017-10-12 15:55:18 +0100

    Add files via upload

---
+++ WalkingDirectory.py
@@ -0,0 +1,12 @@
+#Script that searches a given directory and lists all the subfolders and files within
+import os
+
+for folderName, subfolders, filenames in os.walk('/Users/gmcauley/Desktop/Code/'):#Add directory here
+    print('The current folder is ' + folderName)
+
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+    for filename in filenames:
+        print('FILE INSIDE ' + folderName + ': '+ filename)
+
+    print('')
