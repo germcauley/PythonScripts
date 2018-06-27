e86cd9f95f8cf2f63a139fb07b0c98e6f04ab8ff
Gerald McAuley <geraldmcauley@gmail.com>
2018-06-27 23:06:16 +0100

    Add files via upload

--- draftTests.py
+++ draftTests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import os, sys
 from selenium.webdriver.common.action_chains import ActionChains
 from ScreenShotCompare import ScreenAnalysis
 #set up driver
@@ -10,6 +11,8 @@ def TestMegaMenu(urls):
     for url in urls:
         driver.get(url)
         print('Running tests on '+ str(url))
+        path = '/Users/gmcauley/PycharmProjects/PageObjectModelExample'
+        #os.makedirs(path+url)
         #open window
         # driver.execute_script("window.open('https://twitter.com')")
         # print (driver.current_window_handle)
@@ -31,55 +34,91 @@ def TestMegaMenu(urls):
         hov = ActionChains(driver).move_to_element(element)
         hov.perform()
 
-        time.sleep(0.2)
+        time.sleep(2)
         # Display choices under Products
         for x in range(2,12):
             # print('PRINTING ITEM '+str(x)+ (item[x].get_attribute('innerHTML')))
-            print('PRINTING ITEM ' + str(x) + (item[x].get_attribute('innerHTML')))
+            #print('PRINTING ITEM ' + str(x) + (item[x].get_attribute('innerHTML')))
             item[x].click()
-            #take screenshot
-            driver.save_screenshot('screenshots/ProductsImage'+str(x)+'.png')
-            time.sleep(0.5)
+            #TAKE SCREENSHOT
+
+            try:
+                driver.save_screenshot(os.path.join(os.path.dirname(os.path.realpath(__file__)),'screenshots','ProductsImage'+str(x)+'.png'))
+                driver.get_screenshot_as_png()
+                print("Done.")
+                time.sleep(0.8)
+            except:
+                print('failure!')
 
         ######################################
         #Services hover
         Service_element = driver.find_element_by_xpath('/html/body/nav/div/ul[1]/li[3]')
         hov = ActionChains(driver).move_to_element(Service_element)
         hov.perform()
-        driver.save_screenshot('screenshots/ServicesImage'+str(x)+'.png')
+        # TAKE SCREENSHOT
+        try:
+            driver.save_screenshot(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'screenshots','ServiceImage.png'))
+            driver.get_screenshot_as_png()
+            print("Done.")
+            time.sleep(0.5)
+        except:
+            print('failure!')
         time.sleep(0.5)
 
         #Life Moments
         LM_element = driver.find_element_by_xpath('/html/body/nav/div/ul[1]/li[4]')
         hov = ActionChains(driver).move_to_element(LM_element)
         hov.perform()
-        driver.save_screenshot('screenshots/LifeMomentsImage'+str(x)+'.png')
-        time.sleep(0.5)
-
+        # TAKE SCREENSHOT
+        try:
+            driver.save_screenshot(
+                os.path.join(os.path.dirname(os.path.realpath(__file__)), 'screenshots', 'LifeMomentsImage.png'))
+            driver.get_screenshot_as_png()
+            print("Done.")
+            time.sleep(0.5)
+        except:
+            print('failure!')
+        time.sleep(1)
+########REQUIRES REFACTOR####################
         #Way to Bank
         WTB_element = driver.find_element_by_xpath('/html/body/nav/div/ul[1]/li[5]')
         hov = ActionChains(driver).move_to_element(WTB_element)
         hov.perform()
-        driver.save_screenshot('screenshots/WaysToBankImage.png')
-        time.sleep(0.5)
+        screenShot('WaysToBankImage1.png')
         menu_item2 = driver.find_element_by_xpath('/html/body/nav/div/ul[1]/li[5]/div/div[1]/ul/li[2]')
         menu_item2.click()
-        driver.save_screenshot('screenshots/WaysToBankImage2.png')
-        time.sleep(0.5)
+        screenShot('WaysToBankImage2.png')
+        time.sleep(0.8)
         menu_item3 = driver.find_element_by_xpath('/html/body/nav/div/ul[1]/li[5]/div/div[1]/ul/li[3]/a')
         menu_item3.click()
-        driver.save_screenshot('screenshots/WaysToBankImage3.png')
-        time.sleep(0.5)
+        screenShot('WaysToBankImage3.png')
+        time.sleep(0.8)
 
         ##############################
+
+##Ways to bank screenshot
+
+def screenShot(name):
+     # TAKE SCREENSHOT
+            try:
+                driver.save_screenshot(
+                    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'screenshots', name))
+                driver.get_screenshot_as_png()
+                print("Done.")
+                time.sleep(0.5)
+            except:
+                print('failure!')
+            time.sleep(0.5)
+
+
+
 
 
 urls = ['https://stg.bankofireland.com/','https://stg.bankofireland.com/']
 TestMegaMenu(urls)
+driver.quit()
 
-# Create unique screenshot names relative to url being used
 
 # CALL SCREENSHOT COMPARISON ON STG AND LIVE IMAGEs
-
-THING = ScreenAnalysis('/Users/gmcauley/PycharmProjects/PageObjectModelExample/venv/screenshots/ProductsImage2.png','/Users/gmcauley/PycharmProjects/PageObjectModelExample/venv/screenshots/ProductsImage5.png')
-        THING.analyze()
+THING = ScreenAnalysis('/Users/gmcauley/PycharmProjects/PageObjectModelExample/screenshots/ProductsImage2.png','/Users/gmcauley/PycharmProjects/PageObjectModelExample/screenshots/ProductsImage5.png')
+THING.analyze()
